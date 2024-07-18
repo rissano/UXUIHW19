dc014941ded3ced5f24d4ec1b3ea9a139b2749de
rissano <160439567+rissano@users.noreply.github.com>
2024-07-18 09:39:40 -0400

    animations

--- js/index.js
+++ js/index.js
@@ -1,12 +1,30 @@
 $(document).ready(function() {
     $('.navigation nav ul li a').hover(
         function() {
-            // Mouse enter event
+            // Mouse enter 
             $(this).addClass('hovered');
         }, 
         function() {
-            // Mouse leave event
+            // Mouse leave 
             $(this).removeClass('hovered');
         }
     );
 });
+
+$(document).ready(function(){
+    $('.gridItem').hover(
+      function() {
+        // On mouse
+        let img = $(this).find('.grid-img');
+        let originalSrc = img.attr('src');
+        let newSrc = originalSrc.replace('still.jpeg', 'hover.gif');
+        img.data('original-src', originalSrc);
+        img.attr('src', newSrc);
+      },
+      function() {
+        // On mouse
+        let img = $(this).find('.grid-img');
+        img.attr('src', img.data('original-src'));
+      }
+    );
+  });
